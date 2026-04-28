bbe0a2dcb615e9a6a940880a095eda32bd800a0f
0w1y <sh1569@scarletmail.rutgers.edu>
2026-04-27 21:02:14 -0400

    rough-draft wip realtime

--- rough-draft/js/script.js
+++ rough-draft/js/script.js
@@ -1,45 +1,52 @@
 const clock = document.querySelector('.circle');
 const gear = document.querySelector('.gear');
+const gears = document.querySelectorAll('.gear');
 const hand = document.querySelector('.clock-hand');
+const handSmall = document.querySelector('.smallclock-hand');
 const topIcon = document.querySelector(".dot.top.icon");
 const leftIcon = document.querySelector(".dot.left.icon");
 const rightIcon = document.querySelector(".dot.right.icon");
 const bottomIcon = document.querySelector(".dot.bottom.icon");
-const colors = ["#272f3d", "#b19157", "#dbd27f", "#815847"];
-let colorIndex = 0;
-let degrees = 0;
+const degs = 0;
 
-clock.addEventListener("click", function () {
-    degrees += 90;
-    hand.style.transform = `rotate(${degrees}deg)`;
-    gear.style.transform = `translate(-50%, -50%) rotate(${degrees}deg)`;
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours() % 12;
+    const hour = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
 
-    colorIndex = (colorIndex + 1) % colors.length;
-        document.body.style.backgroundColor = colors[colorIndex];
+    const hourDeg = (hours * 30) + (minutes * 0.5) - 180;
+    const minuteDeg = (minutes * 6) + (seconds * 0.1) - 180;
+    console.log(now.getHours());
+
+    hand.style.transform = `rotate(${hourDeg}deg)`;
+    handSmall.style.transform = `rotate(${minuteDeg}deg)`;
 
     topIcon.classList.remove("glow");
     rightIcon.classList.remove("glow");
     bottomIcon.classList.remove("glow");
     leftIcon.classList.remove("glow");
 
-    switch (degrees % 360) {
-    case 0:
-        bottomIcon.classList.toggle("glow");
-        console.log("0 degrees");
-        break;
-    case 90:
-        leftIcon.classList.toggle("glow");
-        console.log("90 degrees");
-        break;
-    case 180:
-        topIcon.classList.toggle("glow");
-        console.log("180 degrees");
-        break;
-    case 270: 
-        rightIcon.classList.toggle("glow");
-        console.log("270 degrees");
-        break;
+    if (hour >= 21 || hour < 5) {
+        bottomIcon.classList.add("glow");
+    } else if (hour >= 5 && hour < 11) {
+        leftIcon.classList.add("glow");
+    } else if (hour >= 11 && hour < 17) {
+        topIcon.classList.add("glow");
+    } else if (hour >= 17 && hour < 21) {
+        rightIcon.classList.add("glow");
     }
-});
+}
 
-//rotateHand();
+function updateGears() {
+    degs += 20;
+    gears.forEach(gear => {
+        gear.style.transform = `translate(-50%, -50%) rotate(${degs}deg)`;
+    });
+}
+
+updateClock();
+updateGears();
+setInterval(updateClock, 1000);
+setInterval(updateGears, 100);
